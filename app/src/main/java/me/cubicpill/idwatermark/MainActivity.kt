68eb2d2f8bd675bcae5c8dbdf8181bd67b069284
CubicPill <cubicpill@gmail.com>
2019-02-25 22:21:32 +0800

    change api version

--- app/src/main/java/me/cubicpill/idwatermark/MainActivity.kt
+++ app/src/main/java/me/cubicpill/idwatermark/MainActivity.kt
@@ -2,13 +2,25 @@ package me.cubicpill.idwatermark
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val galleryButton = findViewById<Button>(R.id.galleryBtn)
+        val cameraButton = findViewById<Button>(R.id.cameraBtn)
+        
     }
 
-    
+    fun onCameraButtonClick() {
+
+    }
+
+    fun onGalleryButtonClick() {
+
+    }
+
+
 }
