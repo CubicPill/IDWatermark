cc8b5322d7b57c025211ef2d5bed5b6d5d700ea6
CubicPill <cubicpill@gmail.com>
2019-02-28 22:26:58 +0800

    crop

--- app/src/main/java/me/cubicpill/idwatermark/MainActivity.kt
+++ app/src/main/java/me/cubicpill/idwatermark/MainActivity.kt
@@ -1,17 +1,25 @@
 package me.cubicpill.idwatermark
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import java.io.File
+import androidx.core.content.FileProvider
+import com.theartofdev.edmodo.cropper.CropImage
+import android.R.attr.data
+import com.theartofdev.edmodo.cropper.CropImageView
 
 
 class MainActivity : AppCompatActivity() {
     private val REQUEST_CAMERA = 1
     private val REQUEST_GALLERY = 2
-    private val REQUEST_EDITOR = 3
+    private var currentPhotoUri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,11 +34,22 @@ class MainActivity : AppCompatActivity() {
         }
         galleryButton.setOnClickListener(clickListener)
         cameraButton.setOnClickListener(clickListener)
+
     }
 
     fun onCameraButtonClick() {
+        val cameraPhoto = File.createTempFile("ID_WM", null, this.cacheDir)
+        Log.d("IDWatermark", "cameraPhoto uri " + cameraPhoto.toURI().toString())
         val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        val photoUri = FileProvider.getUriForFile(
+                this,
+                "$packageName.fileprovider",
+                cameraPhoto)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+        currentPhotoUri = photoUri
         if (cameraIntent.resolveActivity(packageManager) != null) {
+
             startActivityForResult(cameraIntent, REQUEST_CAMERA)
         }
     }
@@ -38,6 +57,42 @@ class MainActivity : AppCompatActivity() {
     fun onGalleryButtonClick() {
         val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
         startActivityForResult(galleryIntent, REQUEST_GALLERY)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        Log.d("IDWatermark", "onActivityResult")
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            Log.d("IDWatermark", "Result: OK!")
+            when (requestCode) {
+                REQUEST_CAMERA -> {
+                    Log.d("IDWatermark", "URI from camera: " + currentPhotoUri.toString())
+                    CropImage.activity(currentPhotoUri)
+                            .setInitialCropWindowPaddingRatio(0f)
+                            .setGuidelines(CropImageView.Guidelines.OFF)
+                            .start(this)
+
+                }
+                REQUEST_GALLERY -> {
+
+                    Log.d("IDWatermark", "URI from gallery " + data?.data.toString())
+                    currentPhotoUri = data?.data
+                    CropImage.activity(currentPhotoUri)
+                            .setInitialCropWindowPaddingRatio(0f)
+                            .setGuidelines(CropImageView.Guidelines.OFF)
+                            .start(this)
+                }
+                CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE -> {
+                    val result = CropImage.getActivityResult(data)
+                    val resultUri = result.uri
+                    Log.d("IDWatermark", "CropImage result uri: $resultUri")
+                    currentPhotoUri = resultUri
+
+                }
+            }
+        }
 
     }
 
---
+++ app/src/main/java/me/cubicpill/idwatermark/WatermarkActivity.kt
@@ -0,0 +1,12 @@
+package me.cubicpill.idwatermark
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WatermarkActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_watermark)
+    }
+}
